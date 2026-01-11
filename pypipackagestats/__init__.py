7c3af8f61f18044a1be522520508f3187464b368
Siva Sai <26503640+ysskrishna@users.noreply.github.com>
2026-01-11 15:22:44 +0530

    Siva | add in init file

--- pypipackagestats/__init__.py
+++ pypipackagestats/__init__.py
@@ -1,2 +1,6 @@
 """PyPI Package Stats - PyPI Package Information CLI"""
 
+from pypipackagestats.service import PackageStatsService
+from pypipackagestats.core.client import PyPIClient
+
+__all__ = ["PackageStatsService", "PyPIClient"]
